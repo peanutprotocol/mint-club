90f26ff6737020d28fa9727206ecd91c1d4e92bd
Hugo Montenegro <h@hugomontenegro.com>
2022-09-14 17:26:17 +0200

    mainnet launchhhhh lfggg 🔥

--- app/src/js/colors.js
+++ app/src/js/colors.js
@@ -1,5 +1,10 @@
 const root = document.documentElement;
 
+var x = 100;
+var y = 28;
+root.style.setProperty('--h', x)
+root.style.setProperty('--d', x-180)
+
 root.addEventListener('mousemove', e => {
     let x = e.clientX / innerWidth,
         y = e.clientY / innerWidth;
